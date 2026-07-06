108c113317637134959dcd29c0786bcd53a3ecad
mighty-stack <alabiolumide38@gmail.com>
2026-07-06 19:24:20 +0100

    server url

--- src/Services/api.js
+++ src/Services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "https://bearilly-server.onrender.com/api";
+  import.meta.env.VITE_API_BASE_URL;
 
 const TOKEN_KEY =
   import.meta.env.VITE_TOKEN_KEY || "Bearilly_auth_token";
